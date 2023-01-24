139a43be34db27eec6765a213202e3b975460386
New Panuwat <new_sbh@me.com>
2023-01-24 20:32:56 +0700

    add redux store todos

---
+++ src/libs/todoSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+};
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+    },
+    setComplete: (state, action) => {
+      const todo = state.items.find((todo) => todo.id === action.payload);
+      console.log(todo.completed);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+  },
+});
+
+export const { addItem, setComplete } = todoSlice.actions;
+
+export const getTodos = (state) => state.todos.items;
+
+export default todoSlice.reducer;
